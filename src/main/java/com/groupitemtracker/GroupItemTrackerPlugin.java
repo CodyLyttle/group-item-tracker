af2089c8cc846622b762aa7361097bce48a58866
Cody Lyttle <codylyttle123@gmail.com>
2025-06-15 14:58:31 +1200

    Add custom bank search filter.

--- src/main/java/com/groupitemtracker/GroupItemTrackerPlugin.java
+++ src/main/java/com/groupitemtracker/GroupItemTrackerPlugin.java
@@ -7,6 +7,7 @@ import net.runelite.api.Client;
 import net.runelite.api.Menu;
 import net.runelite.api.MenuEntry;
 import net.runelite.api.events.MenuEntryAdded;
+import net.runelite.api.events.ScriptCallbackEvent;
 import net.runelite.api.gameval.InterfaceID;
 import net.runelite.client.config.ConfigManager;
 import net.runelite.client.eventbus.Subscribe;
@@ -17,6 +18,8 @@ import net.runelite.client.plugins.PluginDescriptor;
 @PluginDescriptor(name = "Group Item Tracker")
 public class GroupItemTrackerPlugin extends Plugin
 {
+	private static final String BANK_SEARCH_KEYWORD = "/g";
+	private static final String BANK_SEARCH_KEYWORD_HINT = "<br>" + "Use " + BANK_SEARCH_KEYWORD + " to filter by group item tracker";
 	private static final String MENU_OPTION_ADD = "Add to GIM item tracker";
 	private static final String MENU_OPTION_REMOVE = "Remove from GIM item tracker";
 
@@ -61,6 +64,36 @@ public class GroupItemTrackerPlugin extends Plugin
 					itemTracker.addItem(itemID);
 				}
 			});
+		}
+	}
+
+	@Subscribe(priority = -1) // Force callback to run after other plugins, specifically bank-tags.
+	public void onScriptCallbackEvent(ScriptCallbackEvent event)
+	{
+		final String eventName = event.getEventName();
+		final Object[] stringStack = client.getObjectStack();
+		final int stringStackSize = client.getObjectStackSize();
+
+		switch (eventName)
+		{
+			// Append bank search keyword hint.
+			case "setSearchBankInputText":
+			case "setSearchBankInputTextFound":
+				stringStack[stringStackSize - 1] = stringStack[stringStackSize - 1] + BANK_SEARCH_KEYWORD_HINT;
+				break;
+			// Bank search keyword overrides filter to display tracked items.
+			case "bankSearchFilter":
+				final String searchFilter = (String) stringStack[stringStackSize - 1];
+				if (searchFilter.equals(BANK_SEARCH_KEYWORD))
+				{
+					final int[] intStack = client.getIntStack();
+					final int intStackSize = client.getIntStackSize();
+					final int itemID = intStack[intStackSize - 1];
+
+					// Whether the item should be included in the search results.
+					intStack[intStackSize - 2] = itemTracker.containsItem(itemID) ? 1 : 0;
+				}
+				break;
 		}
 	}
 }
