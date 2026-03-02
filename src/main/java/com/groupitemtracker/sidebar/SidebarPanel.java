ca1ed601bc124d3648874e54cd04935f6c789b81
Cody Lyttle <codylyttle123@gmail.com>
2026-03-02 19:05:40 +1300

    Final sidebar cleanup

--- src/main/java/com/groupitemtracker/sidebar/SidebarPanel.java
+++ src/main/java/com/groupitemtracker/sidebar/SidebarPanel.java
@@ -7,12 +7,11 @@ import com.groupitemtracker.events.ItemRemoved;
 import com.groupitemtracker.events.ItemUpdated;
 import java.awt.BorderLayout;
 import java.awt.Color;
-import java.awt.Dimension;
+import java.awt.Font;
 import java.awt.GridLayout;
 import java.util.Comparator;
 import java.util.TreeMap;
 import javax.swing.BorderFactory;
-import javax.swing.BoxLayout;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
@@ -34,68 +33,78 @@ public class SidebarPanel extends PluginPanel
 	private static final String LOGIN_HINT_LABEL = "Login to view your tracked items";
 	private static final String TUTORIAL_HINT_LABEL = "Right-click bank item to track";
 	private static final String INITIAL_SYNC_HINT_LABEL = "Open bank to finish syncing";
+	private static final String DEFAULT_TRACKED_ITEMS_TEXT = "Tracked Items";
 	private static final Comparator<TrackedItem> ITEM_COMPARER = Comparator.comparing(TrackedItem::getItemName);
-	private static final Color TEXT_COLOR_CLAIMED = ColorScheme.BRAND_ORANGE;
-	private static final Color TEXT_COLOR_UNCLAIMED = ColorScheme.TEXT_COLOR;
 
 	private final ClientThread clientThread;
 	private final ItemManager itemManager;
 	private final ItemTracker itemTracker;
-	private final TreeMap<TrackedItem, ItemPanel> claimedItems = new TreeMap<>(ITEM_COMPARER);
-	private final TreeMap<TrackedItem, ItemPanel> unclaimedItems = new TreeMap<>(ITEM_COMPARER);
-
+	private final TreeMap<TrackedItem, TrackedItemPanel> claimedItems = new TreeMap<>(ITEM_COMPARER);
+	private final TreeMap<TrackedItem, TrackedItemPanel> unclaimedItems = new TreeMap<>(ITEM_COMPARER);
 	private final JLabel hintLabel;
-	private final JPanel trackedItemsPanel;
-	private final JLabel trackedItemsLabel;
-	private final JPanel itemPanelContainer;
+	private final JPanel itemsPanel;
+	private final JLabel itemsHeaderLabel;
+	private final JPanel itemsGrid;
 
 	private boolean isSyncedWithBank;
 
 	public SidebarPanel(ClientThread clientThread, ItemManager itemManager, ItemTracker itemTracker)
 	{
+		// Disable scrolling of the top level panel.
 		super(false);
+		
 		this.clientThread = clientThread;
 		this.itemManager = itemManager;
 		this.itemTracker = itemTracker;
 
-		final var headerPanel = new JPanel(new BorderLayout());
-		headerPanel.setBorder(BorderFactory.createEmptyBorder(8, 12, 8, 12));
-		headerPanel.setBackground(ColorScheme.BORDER_COLOR);
-		final var header = new JLabel("Group Item Tracker");
-		header.setFont(FontManager.getRunescapeFont());
-		header.setHorizontalAlignment(SwingConstants.CENTER);
-		hintLabel = new JLabel(LOGIN_HINT_LABEL);
-		hintLabel.setFont(FontManager.getRunescapeSmallFont());
-		hintLabel.setHorizontalAlignment(SwingConstants.CENTER);
-		headerPanel.add(header, BorderLayout.NORTH);
-		headerPanel.add(hintLabel, BorderLayout.SOUTH);
-
-		trackedItemsPanel = new JPanel(new BorderLayout());
-		trackedItemsPanel.setBorder(BorderFactory.createEmptyBorder(6, 0, 0, 0));
-		final var trackedItemsHeaderPanel = new JPanel(new BorderLayout());
-		trackedItemsHeaderPanel.setBorder(BorderFactory.createEmptyBorder(8, 12, 8, 12));
-		trackedItemsHeaderPanel.setBackground(ColorScheme.BORDER_COLOR);
-		trackedItemsLabel = new JLabel();
-		trackedItemsLabel.setHorizontalAlignment(SwingConstants.CENTER);
-		trackedItemsLabel.setFont(FontManager.getRunescapeFont());
-		trackedItemsHeaderPanel.add(trackedItemsLabel);
-		itemPanelContainer = new JPanel(new GridLayout(0, 1, 0, 1));
-		final var itemPanelContainerWrapper = new JPanel(new BorderLayout());
-		itemPanelContainerWrapper.add(itemPanelContainer, BorderLayout.NORTH);
-		final var itemScrollPane = new JScrollPane(itemPanelContainerWrapper);
-		itemScrollPane.setHorizontalScrollBarPolicy(ScrollPaneConstants.HORIZONTAL_SCROLLBAR_NEVER);
-		itemScrollPane.setBackground(ColorScheme.BORDER_COLOR);
-		trackedItemsPanel.add(trackedItemsHeaderPanel, BorderLayout.NORTH);
-		trackedItemsPanel.add(itemScrollPane, BorderLayout.CENTER);
-
 		setLayout(new BorderLayout());
 		setBorder(BorderFactory.createEmptyBorder(6, 6, 6, 6));
-		add(headerPanel, BorderLayout.NORTH);
-		add(trackedItemsPanel, BorderLayout.CENTER);
-		
-		// Hidden until logged in.
-		trackedItemsPanel.setVisible(false);
 
+		final var titlePanel = createHeaderPanel();
+		final var titleLabel = createHeaderLabel("Group Item Tracker", FontManager.getRunescapeFont());
+		hintLabel = createHeaderLabel(LOGIN_HINT_LABEL, FontManager.getRunescapeSmallFont());
+		titlePanel.add(titleLabel, BorderLayout.NORTH);
+		titlePanel.add(hintLabel, BorderLayout.SOUTH);
+
+		itemsPanel = new JPanel(new BorderLayout());
+		itemsPanel.setBorder(BorderFactory.createEmptyBorder(6, 0, 0, 0));
+		final var itemsHeaderPanel = createHeaderPanel();
+		itemsHeaderLabel = createHeaderLabel(DEFAULT_TRACKED_ITEMS_TEXT, FontManager.getRunescapeFont());
+		itemsHeaderPanel.add(itemsHeaderLabel);
+		// Grid holds all TrackedItemPanel elements.
+		itemsGrid = new JPanel(new GridLayout(0, 1, 0, 1));
+		// Wrapper prevents scroll pane from stretching grid elements vertically.
+		final var gridWrapper = new JPanel(new BorderLayout());
+		gridWrapper.add(itemsGrid, BorderLayout.NORTH);
+		// Scrollable grid overflow when elements exceed available sidebar height.
+		final var itemsScrollPane = new JScrollPane(gridWrapper);
+		itemsScrollPane.setHorizontalScrollBarPolicy(ScrollPaneConstants.HORIZONTAL_SCROLLBAR_NEVER);
+		itemsScrollPane.setBorder(BorderFactory.createMatteBorder(2, 0, 0, 0, ColorScheme.DARK_GRAY_COLOR));
+
+		itemsPanel.add(itemsHeaderPanel, BorderLayout.NORTH);
+		itemsPanel.add(itemsScrollPane, BorderLayout.CENTER);
+
+		add(titlePanel, BorderLayout.NORTH);
+		add(itemsPanel, BorderLayout.CENTER);
+
+		// Hidden until logged in.
+		itemsPanel.setVisible(false);
+	}
+
+	private JPanel createHeaderPanel()
+	{
+		final var panel = new JPanel(new BorderLayout());
+		panel.setBorder(BorderFactory.createEmptyBorder(8, 12, 8, 12));
+		panel.setBackground(ColorScheme.BORDER_COLOR);
+		return panel;
+	}
+
+	private JLabel createHeaderLabel(String text, Font font)
+	{
+		final var label = new JLabel(text);
+		label.setFont(font);
+		label.setHorizontalAlignment(SwingConstants.CENTER);
+		return label;
 	}
 
 	public void login(boolean isBankOpen)
@@ -118,7 +127,7 @@ public class SidebarPanel extends PluginPanel
 				addItemPanel(item);
 			}
 
-			trackedItemsPanel.setVisible(true);
+			itemsPanel.setVisible(true);
 			refreshTrackedItemsPanel();
 		});
 	}
@@ -131,8 +140,8 @@ public class SidebarPanel extends PluginPanel
 
 			claimedItems.clear();
 			unclaimedItems.clear();
-			itemPanelContainer.removeAll();
-			trackedItemsPanel.setVisible(false);
+			itemsGrid.removeAll();
+			itemsPanel.setVisible(false);
 		});
 	}
 
@@ -152,7 +161,6 @@ public class SidebarPanel extends PluginPanel
 		SwingUtilities.invokeLater(() ->
 		{
 			addItemPanel(event.getItem());
-
 			refreshTrackedItemsPanel();
 		});
 	}
@@ -162,10 +170,10 @@ public class SidebarPanel extends PluginPanel
 	{
 		SwingUtilities.invokeLater(() ->
 		{
-			TrackedItem item = event.getItem();
-			TreeMap<TrackedItem, ItemPanel> itemCollection = getCollectionForItem(item);
-			ItemPanel itemPanel = itemCollection.remove(item);
-			itemPanelContainer.remove(itemPanel);
+			final TrackedItem item = event.getItem();
+			final TreeMap<TrackedItem, TrackedItemPanel> itemCollection = getCollectionForItem(item);
+			final TrackedItemPanel trackedItemPanel = itemCollection.remove(item);
+			itemsGrid.remove(trackedItemPanel);
 
 			refreshTrackedItemsPanel();
 		});
@@ -179,30 +187,28 @@ public class SidebarPanel extends PluginPanel
 		SwingUtilities.invokeLater(() ->
 		{
 			final TrackedItem item = event.getItem();
-			final TreeMap<TrackedItem, ItemPanel> itemCollection = getCollectionForItem(item);
-			final ItemPanel itemPanel = itemCollection.get(item);
+			final TreeMap<TrackedItem, TrackedItemPanel> itemCollection = getCollectionForItem(item);
+			final TrackedItemPanel trackedItemPanel = itemCollection.get(item);
 
 			// This is always updated.
-			itemPanel.refresh();
+			trackedItemPanel.refresh();
 
 			if (itemCollection == claimedItems && item.getTotalCount() == 0)
 			{
 				claimedItems.remove(item);
-				unclaimedItems.put(item, itemPanel);
-				int sortedIndex = claimedItems.size() + unclaimedItems.headMap(item).size();
-				itemPanelContainer.remove(itemPanel);
-				itemPanelContainer.add(itemPanel, sortedIndex);
-
+				unclaimedItems.put(item, trackedItemPanel);
+				final int sortedIndex = claimedItems.size() + unclaimedItems.headMap(item).size();
+				itemsGrid.remove(trackedItemPanel);
+				itemsGrid.add(trackedItemPanel, sortedIndex);
 				refreshTrackedItemsPanel();
 			}
 			else if (itemCollection == unclaimedItems && item.getTotalCount() > 0)
 			{
 				unclaimedItems.remove(item);
-				claimedItems.put(item, itemPanel);
-				int sortedIndex = claimedItems.headMap(item).size();
-				itemPanelContainer.remove(itemPanel);
-				itemPanelContainer.add(itemPanel, sortedIndex);
-
+				claimedItems.put(item, trackedItemPanel);
+				final int sortedIndex = claimedItems.headMap(item).size();
+				itemsGrid.remove(trackedItemPanel);
+				itemsGrid.add(trackedItemPanel, sortedIndex);
 				refreshTrackedItemsPanel();
 			}
 		});
@@ -213,10 +219,10 @@ public class SidebarPanel extends PluginPanel
 		assert !(claimedItems.containsKey(item) || unclaimedItems.containsKey(item));
 
 		final AsyncBufferedImage image = itemManager.getImage(item.getItemID(), Integer.MAX_VALUE, false);
-		ItemPanel itemPanel = new ItemPanel(clientThread, itemTracker, item, image);
+		TrackedItemPanel trackedItemPanel = new TrackedItemPanel(clientThread, itemTracker, item, image);
 
-		TreeMap<TrackedItem, ItemPanel> itemCollection = item.getTotalCount() > 0 ? claimedItems : unclaimedItems;
-		itemCollection.put(item, itemPanel);
+		final TreeMap<TrackedItem, TrackedItemPanel> itemCollection = item.getTotalCount() > 0 ? claimedItems : unclaimedItems;
+		itemCollection.put(item, trackedItemPanel);
 
 		int sortedIndex = itemCollection.headMap(item).size();
 		if (itemCollection == unclaimedItems)
@@ -225,10 +231,11 @@ public class SidebarPanel extends PluginPanel
 			sortedIndex += claimedItems.size();
 		}
 
-		itemPanelContainer.add(itemPanel, sortedIndex);
+		itemsGrid.add(trackedItemPanel, sortedIndex);
 	}
 
-	private TreeMap<TrackedItem, ItemPanel> getCollectionForItem(TrackedItem item)
+	// Note: Only call this for items that are guaranteed to be in a collection.
+	private TreeMap<TrackedItem, TrackedItemPanel> getCollectionForItem(TrackedItem item)
 	{
 		if (claimedItems.containsKey(item))
 		{
@@ -241,12 +248,19 @@ public class SidebarPanel extends PluginPanel
 
 	private void refreshTrackedItemsPanel()
 	{
-		int claimed = claimedItems.size();
-		int total = claimed + unclaimedItems.size();
-		trackedItemsLabel.setText("Claimed (" + claimed + "/" + total + ")");
-		trackedItemsLabel.setForeground(claimed > 0 ? TEXT_COLOR_CLAIMED : TEXT_COLOR_UNCLAIMED);
+		final int claimedItemCount = claimedItems.size();
+		if (claimedItemCount == 0)
+		{
+			itemsHeaderLabel.setText(DEFAULT_TRACKED_ITEMS_TEXT);
+			itemsHeaderLabel.setForeground(ColorScheme.TEXT_COLOR);
+		}
+		else
+		{
+			itemsHeaderLabel.setText(DEFAULT_TRACKED_ITEMS_TEXT + " (" + claimedItemCount + ")");
+			itemsHeaderLabel.setForeground(ColorScheme.BRAND_ORANGE);
+		}
 
-		trackedItemsPanel.revalidate();
-		trackedItemsPanel.repaint();
+		itemsPanel.revalidate();
+		itemsPanel.repaint();
 	}
 }
--- src/main/java/com/groupitemtracker/sidebar/TrackedItemPanel.java
+++ src/main/java/com/groupitemtracker/sidebar/TrackedItemPanel.java
@@ -16,7 +16,6 @@ import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
-import lombok.Getter;
 import net.runelite.api.Constants;
 import net.runelite.client.callback.ClientThread;
 import net.runelite.client.ui.ColorScheme;
@@ -27,13 +26,8 @@ import net.runelite.client.util.AsyncBufferedImage;
 import net.runelite.client.util.ImageUtil;
 import net.runelite.client.util.SwingUtil;
 
-public class ItemPanel extends PluginPanel
+public class TrackedItemPanel extends JPanel 
 {
-	private static final Color BACKGROUND_COLOR = ColorScheme.DARKER_GRAY_COLOR;
-	private static final Color TEXT_COLOR_CLAIMED = ColorScheme.BRAND_ORANGE;
-	private static final Color TEXT_COLOR_UNCLAIMED = ColorScheme.TEXT_COLOR;
-	private static final Color TEXT_COLOR_LOCATION_HINT = Color.gray;
-	private static final String DELETE_TOOLTIP = "Stop tracking this item";
 	private static final ImageIcon DELETE_ICON;
 	private static final ImageIcon DELETE_ICON_DIMMED;
 	private static final String[] cachedLocationStrings;
@@ -46,7 +40,7 @@ public class ItemPanel extends PluginPanel
 
 		// Alternate icon for when the button isn't hovered.
 		final float alpha = 0.16f;
-		Image grayImage = GrayFilter.createDisabledImage(DELETE_ICON.getImage());
+		final Image grayImage = GrayFilter.createDisabledImage(DELETE_ICON.getImage());
 		DELETE_ICON_DIMMED = new ImageIcon(ImageUtil.alphaOffset(grayImage, alpha));
 
 		// Preallocate all location strings.
@@ -60,7 +54,7 @@ public class ItemPanel extends PluginPanel
 		final int combinations = (1 << n);
 		cachedLocationStrings = new String[combinations];
 
-		var sb = new StringBuilder();
+		final var sb = new StringBuilder();
 		for (int mask = 0; mask < combinations; mask++)
 		{
 			for (int bit = 0; bit < n; bit++)
@@ -81,15 +75,17 @@ public class ItemPanel extends PluginPanel
 		}
 	}
 
+	private final TrackedItem item;
 	private final JLabel nameLabel;
 	private final JLabel locationsLabel;
 
-	@Getter
-	private final TrackedItem item;
-
-	public ItemPanel(ClientThread clientThread, ItemTracker itemTracker, TrackedItem item, AsyncBufferedImage itemIcon)
+	public TrackedItemPanel(ClientThread clientThread, ItemTracker itemTracker, TrackedItem item, AsyncBufferedImage itemIcon)
 	{
 		this.item = item;
+
+		setLayout(new BorderLayout());
+		setBorder(BorderFactory.createEmptyBorder(6, 6, 6, 6));
+		setBackground(ColorScheme.DARKER_GRAY_COLOR);
 
 		final var icon = new JLabel();
 		icon.setMinimumSize(new Dimension(Constants.ITEM_SPRITE_WIDTH, Constants.ITEM_SPRITE_HEIGHT));
@@ -103,7 +99,7 @@ public class ItemPanel extends PluginPanel
 		nameLabel.setFont(FontManager.getRunescapeFont());
 		locationsLabel = new JLabel();
 		locationsLabel.setFont(FontManager.getRunescapeSmallFont());
-		locationsLabel.setForeground(TEXT_COLOR_LOCATION_HINT);
+		locationsLabel.setForeground(Color.gray);
 		// Vertical glue ensures visible labels stay vertically centered.
 		infoPanel.add(Box.createVerticalGlue());
 		infoPanel.add(nameLabel);
@@ -112,7 +108,6 @@ public class ItemPanel extends PluginPanel
 
 		final var removeButton = new JButton();
 		removeButton.addActionListener((e) -> clientThread.invoke(() -> itemTracker.removeItem(item.getItemID())));
-		removeButton.setToolTipText(DELETE_TOOLTIP);
 		removeButton.setIcon(DELETE_ICON_DIMMED);
 		removeButton.setPressedIcon(DELETE_ICON);
 		removeButton.setRolloverIcon(DELETE_ICON);
@@ -120,27 +115,25 @@ public class ItemPanel extends PluginPanel
 		removeButton.setPreferredSize(new Dimension(30, 30));
 		SwingUtil.removeButtonDecorations(removeButton);
 
-		setLayout(new BorderLayout());
-		setBackground(BACKGROUND_COLOR);
 		add(icon, BorderLayout.WEST);
 		add(infoPanel, BorderLayout.CENTER);
 		add(removeButton, BorderLayout.EAST);
+
 		refresh();
 	}
 
 	public void refresh()
 	{
 		final boolean isClaimed = item.getTotalCount() > 0;
-
 		if (isClaimed)
 		{
-			nameLabel.setForeground(TEXT_COLOR_CLAIMED);
+			nameLabel.setForeground(ColorScheme.BRAND_ORANGE);
 			locationsLabel.setText(getLocationString());
 			locationsLabel.setVisible(true);
 		}
 		else
 		{
-			nameLabel.setForeground(TEXT_COLOR_UNCLAIMED);
+			nameLabel.setForeground(ColorScheme.TEXT_COLOR);
 			locationsLabel.setVisible(false);
 		}
 
